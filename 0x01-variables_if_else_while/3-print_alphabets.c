51134363b25deaf7b4d7c4533006bf89cba5f455
N1biru130 <neville98albert@gmail.com>
2023-07-17 12:49:43 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main -Entry point
+ *
+ * Return: Always(0) success
+ *
+ */
+
+int main(void) 
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+
+	}
+
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
